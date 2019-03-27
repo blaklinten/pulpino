a84b0b690cf921421daeb5831048cc14d0b83d20
Lucas Nilsson <lucasnilsson92@gmail.com>
2019-03-27 15:45:35 +0100

    It compiles, now fix function

--- acc/multiply2.sv
+++ acc/multiply2.sv
@@ -4,9 +4,9 @@ module multiply2 #(
 ) (
   input  logic                start                             ,
   input  logic                clk                               , //kanske ej behövs
-  input  logic [dat_size-1:0] mat_A [mat_size-1:0][mat_size-1:0],
-  input  logic [dat_size-1:0] mat_B [mat_size-1:0][mat_size-1:0],
-  output logic [dat_size-1:0] mat_C [mat_size-1:0][mat_size-1:0],
+  input  logic [3:0][7:0] mat_A [255:0],
+  input  logic [3:0][7:0] mat_B [255:0],
+  output  logic [3:0][7:0] mat_C [255:0],
   output logic                done                            
 );
 
@@ -18,16 +18,15 @@ logic [dat_size-1:0] temp_C [mat_size-1:0][mat_size-1:0] = '{'{8'h00, 8'h00},
 
 always @(posedge start)
   begin
-    for(int i=0;i<mat_size;i++) begin
-      for(int j=0;j<mat_size;j++) begin
-        for(int k=0;k<mat_size;k++) begin
-           temp_C[i][j] = (temp_C[i][j] + (mat_A[i][k] * mat_B[k][j]));
-           $display("%b",temp_C[j][k]);
-        end // for(int k=0;k<mat_size;k++)
-      end // for(int j=0;j<mat_size;j++)
-    end // for(int i=0;i<mat_size;i++
-    mat_C = temp_C;
-    done = 1;
+//  for(int i=0;i<mat_size;i++) begin
+//    for(int j=0;j<mat_size;j++) begin
+//      for(int k=0;k<mat_size;k++) begin
+//         temp_C[i][j] = (temp_C[i][j] + (mat_A[i][k] * mat_B[k][j]));
+//         $display("%b",temp_C[j][k]);
+//      end // for(int k=0;k<mat_size;k++)
+//    end // for(int j=0;j<mat_size;j++)
+//  end // for(int i=0;i<mat_size;i++
+//  mat_C = temp_C;
+//  done = 1;
   end
 endmodule
-
--- acc/top_acc.sv
+++ acc/top_acc.sv
@@ -1,14 +1,14 @@
 module top_acc 
-  #(
+#(
     parameter dat_size = 8,
     parameter mat_size = 2
   )
-  (
+(
     //input  logic            start            ,
     //output logic            done             ,
-    input  logic [3:0][7:0] acc_out    [255:0],
-    output logic [3:0][7:0] acc_in_A [255:0],
-    output logic [3:0][7:0] acc_in_B [255:0]
+    output  logic [3:0][7:0] acc_out    [255:0],
+    input logic   [3:0][7:0] acc_in_A   [255:0],
+    input logic   [3:0][7:0] acc_in_B   [255:0]
   );
   multiply2 
   #(
@@ -19,9 +19,8 @@ module top_acc
   (
      //   .start  (  start      ),
      //   .done   (  done       ),
-        .mat_C  (  acc_out     ),
+        .mat_C  (  acc_out   ),
         .mat_A  (  acc_in_A  ),
         .mat_B  (  acc_in_B  )
   );
-
-      
+endmodule
