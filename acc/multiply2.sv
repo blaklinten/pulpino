4ace2fb24bba92635fd37e8f909b3f3b73558732
addamoom <adam.stenseke@gmail.com>
2019-03-28 09:53:27 +0100

    massor, multiply kan vara broken

--- acc/multiply2.sv
+++ acc/multiply2.sv
@@ -2,31 +2,56 @@ module multiply2 #(
   parameter mat_size = 2,
   parameter dat_size = 8
 ) (
-  input  logic                start                             ,
-  input  logic                clk                               , //kanske ej behövs
+  input  logic            start        ,
+  input  logic            clk          , //kanske ej behövs
   input  logic [3:0][7:0] mat_A [255:0],
   input  logic [3:0][7:0] mat_B [255:0],
-  output  logic [3:0][7:0] mat_C [255:0],
-  output logic                done                            
+  output logic [3:0][7:0] mat_C [255:0],
+  output logic            done
 );
 
-int                         i, j, k;
+  int i, j, k,l,m,n;
+  int w = 0;
 
-logic [dat_size-1:0] temp_C [mat_size-1:0][mat_size-1:0] = '{'{8'h00, 8'h00},
-                                                              '{8'h00, 8'h00}};
+  logic [dat_size-1:0] temp_C[mat_size-1:0][mat_size-1:0] = '{'{8'h00, 8'h00},'{8'h00, 8'h00}};
+  logic [dat_size-1:0] temp_A[mat_size-1:0][mat_size-1:0]                                     ;
+  logic [dat_size-1:0] temp_B[mat_size-1:0][mat_size-1:0]                                     ;
 
 
-always @(posedge start)
-  begin
-//  for(int i=0;i<mat_size;i++) begin
-//    for(int j=0;j<mat_size;j++) begin
-//      for(int k=0;k<mat_size;k++) begin
-//         temp_C[i][j] = (temp_C[i][j] + (mat_A[i][k] * mat_B[k][j]));
-//         $display("%b",temp_C[j][k]);
-//      end // for(int k=0;k<mat_size;k++)
-//    end // for(int j=0;j<mat_size;j++)
-//  end // for(int i=0;i<mat_size;i++
-//  mat_C = temp_C;
-//  done = 1;
+  always @(posedge start)
+    begin
+
+      for(int l=0;l<mat_size;l++) begin             //vilken rad vi är på
+        for(int m=0;m<mat_size;m=m+4) begin         //vi vill gå mellan var fjärde kolonn
+          for(int n=0;n<mat_size;n++) begin         //stega igenom de 4 bits som fins lagrade i varje ord
+            temp_A[l][m+n] = mat_A[w][n];
+            temp_B[l][m+n] = mat_B[w][n];
+          end
+          w++;                                    //när vi gått igenom 4 bits, gå till nästa ord
+        end
+      end
+
+
+    for(int i=0;i<mat_size;i++) begin
+      for(int j=0;j<mat_size;j++) begin
+        for(int k=0;k<mat_size;k++) begin
+        temp_C[i][j] = (temp_C[i][j] + (temp_A[i][k] * temp_B[k][j]));
+        end
+      end 
+    end 
+    w=0;
+    for(int row = 0; row < mat_size; row++) begin
+      for(int kol =0; kol < mat_size;kol++)
+        if(kol%4==0){
+          w++
+          sec=0
+        }
+        mat_C[row][kol] = temp_C[w][sec];
+        sec++
+      end
+    end
+
+        w++
+  done  = 1;
   end
 endmodule
--- acc/testbench_multiply.sv
+++ acc/testbench_multiply.sv
@@ -1,35 +1,41 @@
-`include "multiply2.sv"
 module testbench_multiply ();
 
-	logic [7:0] mat_A   [1:0][1:0]    ;
-	logic [7:0] mat_B   [1:0][1:0]    ;
-	logic [7:0] mat_C   [1:0][1:0]    ;
-	logic start;
-	logic done;
-	logic clk;
 
-	multiply2 mult(
-                .clk (clk),
+	logic [3:0][7:0]          mat_A[255:0];
+	logic [3:0][7:0]          mat_B[255:0];
+	logic [3:0][7:0]          mat_C[255:0];
+	logic                     start       ;
+	logic                     done        ;
+	logic                     clk         ;
+
+	multiply2 mult (
+		.clk  (clk  ),
 		.start(start),
 		.mat_A(mat_A),
 		.mat_B(mat_B),
 		.mat_C(mat_C),
-		.done(done)
+		.done (done )
 	);
 	initial begin
 		#100
 		mat_A <= '{default:0};
 		mat_B <= '{default:0};
 		mat_C <= '{default:0};
-		mat_C[0][0] = 8'd0; mat_C[0][1] = 8'd0; mat_C[1][0] = 8'd0; mat_C[1][1] = 8'd0;
-		$display("%b",mat_C[0][0]);
+
+		//mat_C[0][0] = 8'd0; mat_C[0][1] = 8'd0; mat_C[1][0] = 8'd0; mat_C[1][1] = 8'd0;
+		//$display("%b",mat_C[0][0]);
+
 		start = '0;
 		done  = '0;
 		#100;
-		mat_A[0][0] = 8'd2; mat_A[0][1] = 8'd2; mat_A[1][0] = 8'd2; mat_A[1][1] = 8'd2;
-		mat_B[0][0] = 8'd128; mat_B[0][1] = 8'd128; mat_B[1][0] = 8'd128; mat_B[1][1] = 8'd128;
+		mat_A[0][0] = 8'd2; mat_A[0][1] = 8'd2; mat_A[0][2] = 8'd2; mat_A[0][3] = 8'd2;
+		mat_B[0][0] = 8'd4; mat_B[0][1] = 8'd4; mat_B[0][2] = 8'd4; mat_B[0][3] = 8'd4;
 		#100;
 		start=1;
 		#200;
+		$display("%p",mat_C[0][0]);
+		$display("%p",mat_C[0][1]);
+		$display("%p",mat_C[0][2]);
+		$display("%p",mat_C[0][3]);
 	end
-	endmodule // testbench_multiply
+endmodule // testbench_multiply
