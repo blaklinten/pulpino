f67ffec9ad465d42f435175e133f33ce1b66aaa7
addamoom <adam.stenseke@gmail.com>
2019-04-26 13:28:58 +0200

    ändrat adresser för att fixa bygge

--- acc/apb_acc.sv
+++ acc/apb_acc.sv
@@ -5,7 +5,7 @@ module apb_acc
 (
   input  logic                      HCLK,
   input  logic                      HRESETn,
-  input  logic [32-1:0] PADDR,
+  input  logic [APB_ADDR_WIDTH-1:0] PADDR,
   input  logic               [31:0] PWDATA,
   input  logic                      PWRITE,
   input  logic                      PSEL,
@@ -34,7 +34,7 @@ top_acc_i
 );
 
 assign clk = HCLK;
-assign addr = PADDR[11:0];
+assign addr = PADDR[11:2];
 
   always_ff @(posedge HCLK, negedge HRESETn)
   begin
@@ -49,30 +49,30 @@ assign addr = PADDR[11:0];
       if (PSEL && PENABLE && PWRITE)
       begin
         case (addr)
-          0:
+          4'b0000000000:
             start <= PWDATA[0];
-          1:
+          4'b0000000001:
           begin
             acc_in_A[0] <= PWDATA[7:0];
             acc_in_A[1] <= PWDATA[15:8];
             acc_in_A[2] <= PWDATA[23:16];
             acc_in_A[3] <= PWDATA[31:24];
           end
-          5:
+          10'b0000000010:
           begin
             acc_in_A[4] <= PWDATA[7:0];
             acc_in_A[5] <= PWDATA[15:8];
             acc_in_A[6] <= PWDATA[23:16];
             acc_in_A[7] <= PWDATA[31:24];
           end
-          1025:
+          10'b0100000001:
           begin
             acc_in_B[0] <= PWDATA[7:0];
             acc_in_B[1] <= PWDATA[15:8];
             acc_in_B[2] <= PWDATA[23:16];
             acc_in_B[3] <= PWDATA[31:24];
           end
-          1029:
+          10'b0100000010:
           begin
             acc_in_B[4] <= PWDATA[7:0];
             acc_in_B[5] <= PWDATA[15:8];
@@ -90,7 +90,7 @@ assign addr = PADDR[11:0];
     case (addr)
       0:
         PRDATA[1]      = done;
-      2049:
+      10'b1000000001:
       begin
         // 2049 = 1024 bytes * 2 ( tredje matrisen) + 1
         PRDATA[7:0]    = acc_out[0];
@@ -98,7 +98,7 @@ assign addr = PADDR[11:0];
         PRDATA[23:16]  = acc_out[2];
         PRDATA[31:24]  = acc_out[3];
       end
-      2053:                
+      10'b1000000010:                
       begin
         PRDATA[7:0]    = acc_out[4];
         PRDATA[15:8]   = acc_out[5];
@@ -106,7 +106,7 @@ assign addr = PADDR[11:0];
         PRDATA[31:24]  = acc_out[7];
       end
       default:
-        PRDATA[7:0]    = 8'b01101;
+        PRDATA[11:0]    = PADDR;
     endcase
   end
 
